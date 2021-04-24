afe0d03401e6141eaabe752a2eda0f81d797467b
Ryan McArdle <rmcardle@hersheys.com>
2021-04-24 18:13:08 -0400

    initial working function for calling spoonify API for random recipes

--- spoonify.py
+++ spoonify.py
@@ -1,13 +1,16 @@
 import requests, json
 
 # Get random recipes
-def get_random_recipes(n, apiKey, verify = True):
-      response = requests.get(
-            url = 'https://api.spoonacular.com/recipes/random?apiKey=' + str(apiKey) + '&number=' + str(n),
-            verify = verify
-      )
+def get_random_recipes(apiKey, n = 100, verify = True):
+      
+      if n > 100: n = 100
+      if n < 1: n = 1
+      
+      url = 'https://api.spoonacular.com/recipes/random?apiKey=' + str(apiKey) + '&number=' + str(int(n))
+      response = requests.get(url = url, verify = verify)
+      
       try:
             recipes = response.json()
             return recipes
       except:
-            print(response.status_code + " - " + response.reason)
+            print(str(response.status_code) + " - " + response.reason)
